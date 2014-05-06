4285c2326e0ff6c3d5f3dd1b018d4c1c3ab913cc
Matt Stump <mrevilgnome@gmail.com>
2014-05-06 15:35:07 -0700

    humidity, temp, and dew point are tested and working

--- read_sensors.py
+++ read_sensors.py
@@ -1,49 +1,41 @@
-import time, sys, ctypes, struct, curses
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time, sys, ctypes, struct, math
 import RPi.GPIO as GPIO
 
+SHT_DATA_PIN      = 24
+SHT_CLOCK_PIN     = 23
 MOTION_SENSOR_PIN = 4
-LED_PIN = 18
+LED_PIN           = 18
+
 
 def setup():
     GPIO.setmode(GPIO.BCM)
     setup_motion_sensor(MOTION_SENSOR_PIN)
 
+
 def setup_motion_sensor(input_pin):
     GPIO.setup(input_pin, GPIO.IN)
+
 
 def motion_sensor(pin):
     return GPIO.input(pin)
 
 
-def shift_out(data_pin, clock_pin, value, lsb_first=True, bits=8, delay_micros=0):
-    data = []
-
-    if type(value) == type(1):
-        data = struct.pack('i', value)
-
-    elif type(value) == type('a'):
-        data = [value]
-
-    elif type(value) == type("a"):
-        data = value
-
-    else:
-        raise Exception("shift_out unsupported type: type(value) => %s" % type(value))
-
-    for i in data:
-        shift_out_byte(data_pin, clock_pin, i, lsb_first, bits, delay_micros)
+def clock_tick(clock_pin, level, delay=0.0000001):
+    GPIO.output(clock_pin, level)
+    time.sleep(delay)
 
 
-def shift_out_byte(data_pin, clock_pin, value, lsb_first=True, bits=8, delay_micros=0):
+def shift_out_byte(data_pin, clock_pin, value, lsb_first=True, bits=8, delay=0.0000001):
     for i in xrange(0, bits):
         if lsb_first:
             GPIO.output(data_pin, bool(value & (1 << i)))
         else:
             GPIO.output(data_pin, bool(value & (1 << ((bits - 1 - i )))))
 
-        GPIO.output(clock_pin, GPIO.HIGH)
-        time.usleep(delay_micros)
-        GPIO.output(clock_pin, GPIO.LOW)
+        clock_tick(clock_pin, GPIO.HIGH)
+        clock_tick(clock_pin, GPIO.LOW)
 
 
 def sht_send_command(data_pin, clock_pin, command):
@@ -51,24 +43,26 @@ def sht_send_command(data_pin, clock_pin, command):
     GPIO.setup(clock_pin, GPIO.OUT)
 
     GPIO.output(data_pin, GPIO.HIGH)
-    GPIO.output(clock_pin, GPIO.HIGH)
+    clock_tick(clock_pin, GPIO.HIGH)
 
     GPIO.output(data_pin, GPIO.LOW)
-    GPIO.output(clock_pin, GPIO.LOW)
-
-    GPIO.output(clock_pin, GPIO.HIGH)
+    clock_tick(clock_pin, GPIO.LOW)
+    clock_tick(clock_pin, GPIO.HIGH)
     GPIO.output(data_pin, GPIO.HIGH)
-    GPIO.output(clock_pin, GPIO.LOW)
-    shift_out(data_pin, clock_pin, False, command)
+    clock_tick(clock_pin, GPIO.LOW)
+
+    shift_out_byte(data_pin, clock_pin, command, False)
+    GPIO.output(clock_pin, GPIO.HIGH)
 
     GPIO.setup(data_pin, GPIO.IN)
-    GPIO.output(clock_pin, GPIO.HIGH)
-    if GPIO.input(pin):
-        print "ACK error 0"
+    ack = GPIO.input(data_pin)
+    if ack != GPIO.LOW:
+        raise Exception("SHT15 command ACK failure 1")
 
     GPIO.output(clock_pin, GPIO.LOW)
-    if GPIO.input(pin):
-        print "ACK error 1"
+    ack = GPIO.input(data_pin)
+    if ack != GPIO.HIGH:
+        raise Exception("SHT15 command ACK failure 2")
 
 
 def sht_wait_result(data_pin):
@@ -76,15 +70,15 @@ def sht_wait_result(data_pin):
 
     # wait for the SHTx answer
     # need to wait up to 2 seconds for the value
-    ack = True
-    for i in xrange(0, 1000):
-        time.usleep(2)
+    ack = GPIO.HIGH
+    for i in xrange(0, 100):
+        time.sleep(.01)
         ack = GPIO.input(data_pin)
-        if ack == False:
+        if ack == GPIO.LOW:
             break
 
-    if ack == True:
-        print "ACK error 2"
+    if ack == GPIO.HIGH:
+        raise Exception("SHT15 not responding with data")
 
 
 def shift_in_byte(data_pin, clock_pin, lsb_first = True):
@@ -121,7 +115,7 @@ def sht_get_data(data_pin, clock_pin):
     GPIO.output(clock_pin, GPIO.LOW)
 
     # get the LSB (less significant bits)
-    GPIO.setup(data_pin, INPUT)
+    GPIO.setup(data_pin, GPIO.IN)
     LSB = shift_in_byte(data_pin, clock_pin, False)
     return ((MSB << 8) | LSB)  # combine bits
 
@@ -131,44 +125,70 @@ def sht_skip_crc(data_pin, clock_pin):
     GPIO.setup(data_pin, GPIO.OUT)
     GPIO.setup(clock_pin, GPIO.OUT)
     GPIO.output(data_pin, GPIO.HIGH)
-    GPIO.output(clock_pin, GPIO.HIGH)
-    GPIO.output(clock_pin, GPIO.LOW)
+    clock_tick(clock_pin, GPIO.HIGH)
+    clock_tick(clock_pin, GPIO.LOW)
 
 
 def sht_get_temp(data_pin, clock_pin):
-  # temperature in celsius
-  sht_send_command(B00000011, data_pin, clock_pin)
-  sht_wait_result(data_pin)
+    # temp in celsius
+    sht_send_command(data_pin, clock_pin, int(0x03))
+    sht_wait_result(data_pin)
 
-  val = sht_get_data(data_pin, clock_pin)
-  sht_skip_crc(data_pin, clock_pin)
-  return val * 0.01 - 40 # convert to celsius
+    val = sht_get_data(data_pin, clock_pin)
+    sht_skip_crc(data_pin, clock_pin)
+    return val * 0.01 - 40 # convert to celsius
 
 
-def sht_get_humidity(data_pin, clock_pin):
-  # relative Humidity
-  sht_send_command(B00000101, data_pin, clock_pin)
-  sht_wait_result(data_pin)
+def sht_get_humidity(data_pin, clock_pin, temp):
+    # relative Humidity
+    sht_send_command(data_pin, clock_pin, int(0x05))
+    sht_wait_result(data_pin)
 
-  val = sht_get_data(data_pin, clock_pin)
-  sht_skip_crc(data_pin, clock_pin)
-  return -4.0 + 0.0405 * val + -0.0000028 * val * val
+    raw_humidity = sht_get_data(data_pin, clock_pin)
+    sht_skip_crc(data_pin, clock_pin)
+
+    C1 = -2.0468
+    C2 = 0.0367
+    C3 = -1.5955e-6
+    T1 = 0.01
+    T2 = 0.00008
+    # rh = raw_humidity from sensor
+    # linear_humidty = c1 + c2 * rh + c3 * rh^2
+    # temp_comp_humid = (temp - 25) * (t1 + t2 * rh) + linear_humidity
+
+    linear_humidty = C1 + C2 * raw_humidity + C3 * raw_humidity * raw_humidity
+    temp_comp_humid = (temp - 25.0 ) * (T1 + T2 * raw_humidity) + linear_humidty
+    return temp_comp_humid
+
+
+def calc_dew_point(temp, relative_humidity):
+    # H = (log10(relative_humidity)-2)/0.4343 + (17.62*temp)/(243.12+temp);
+    # DP = 243.12*H/(17.62-H);
+    H = (math.log10(relative_humidity) - 2) / 0.4343 + (17.62 * temp) / (243.12 + temp)
+    DP = 243.12 * H / (17.62 - H)
+    return DP
 
 
 def main():
     try:
-        stdscr = curses.initscr()
+        setup()
 
         while True:
-            motion   = motion_sensor(MOTION_SENSOR_PIN);
-            temp     = sht_get_temp(SHT_DATA_PIN, SHT_CLOCK_PIN)
-            humidity = sht_get_humidity(SHT_DATA_PIN, SHT_CLOCK_PIN)
+            motion    = motion_sensor(MOTION_SENSOR_PIN);
+            temp      = sht_get_temp(SHT_DATA_PIN, SHT_CLOCK_PIN)
+            time.sleep(1)
+            humidity  = sht_get_humidity(SHT_DATA_PIN, SHT_CLOCK_PIN, temp)
+            dew_point = calc_dew_point(temp, humidity)
 
-            stdscr.addstr(0, 0, "motion: %s | temp: %s°C | relative humidity: %s" % (motion, temp, humidity))
-            stdscr.refresh()
+            sys.stdout.write("motion: %s | temp: %06.2f°C | relative humidity: %06.2f | dew point: %06.2f°C\r" % (motion, temp, humidity, 13.9499999))
+            sys.stdout.flush()
             time.sleep(1)
 
     except KeyboardInterrupt:
+        pass
+
+    finally:
+        print ""
         GPIO.cleanup()
 
 
